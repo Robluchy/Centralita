861c228f4354546ca6a4db8b659ab5fb905dc09c
Robluchy <Dionny75@gmail.com>
2023-02-06 20:23:28 +0100

    update all configs

--- Central_Fempa/CentralitaApp/CentralitaAppFinal/obj/Debug/net6.0-windows/Pages/MostrarRegistros.g.i.cs
+++ Central_Fempa/CentralitaApp/CentralitaAppFinal/obj/Debug/net6.0-windows/Pages/MostrarRegistros.g.i.cs
@@ -61,7 +61,7 @@ namespace CentralitaAppFinal {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CentralitaAppFinal;V1.0.0.0;component/pages/mostrarregistros.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CentralitaAppFinal;component/pages/mostrarregistros.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\MostrarRegistros.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
